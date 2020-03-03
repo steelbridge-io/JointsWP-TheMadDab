4c6609b4ef9c37144472d498252d2c10d4bb82e6
steelbridge-io <chris@steelbridge.io>
2020-03-02 19:35:54 -0800

    Updated the overflow:hide for .card under the product menu. Overflow property was hiding the navigation dropdown.

--- functions/menu.php
+++ functions/menu.php
@@ -32,7 +32,7 @@ function joints_product_nav() {
     'menu_class'		=> 'medium-horizontal menu',	// Adding custom nav class
     //'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
 	
-	'items_wrap'        => '<ul id="%1$s" class="%2$s menu vertical nested">%3$s</ul>',
+	'items_wrap'        => '<ul id="%1$s" class="%2$s menu vertical nested" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
     'theme_location'	=> 'product-nav',					// Where it's located in the theme
     'depth'				=> 5,							// Limit the depth of the nav
     'fallback_cb'		=> false,						// Fallback function (see below)
